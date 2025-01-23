9f783b90ce45bb2c3a9120a318c8d87ccffec1be
puraniya Hardik <159144980+puraniyahardik@users.noreply.github.com>
2025-01-23 15:22:47 +0530

    Update CTA.jsx

--- src/componets/CTA.jsx
+++ src/componets/CTA.jsx
@@ -7,7 +7,7 @@ const CTA = () => {
       <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col dark:bg-black-gradient-2 rounded-[20px] box-shadow`}>
         <div className='flex-1 flex flex-col'>
           <h2 className={styles.heading2}>Let's try our services now!</h2>
-          <p  className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim nobis fugiat animi, sunt aspernatur eveniet repellendus voluptatem ea esse quod!</p>
+          <p  className={`${styles.paragraph} max-w-[470px] mt-5 dark:text-white`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim nobis fugiat animi, sunt aspernatur eveniet repellendus voluptatem ea esse quod!</p>
         </div>
 
 
